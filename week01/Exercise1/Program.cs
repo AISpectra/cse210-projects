8c76ffd892e6ad268f0601c2ee2bd76037a4cc61
Samuel <spectra.gethelp@gmail.com>
2025-05-07 21:42:02 +0200

    Actualización

--- week01/Exercise1/Program.cs
+++ week01/Exercise1/Program.cs
@@ -4,6 +4,15 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise1 Project.");
+        // Ask name
+        Console.Write("What is your first name? ");
+        string firstName = Console.ReadLine();
+
+        // Ask surname
+        Console.Write("What is your last name? ");
+        string lastName = Console.ReadLine();
+
+        // Print result
+        Console.WriteLine($"Your name is {lastName}, {firstName} {lastName}.");
     }
 }
